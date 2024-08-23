68beed455ecb90cd6e21c8861698f9bb215fcbee
abdullah012039 <162738815+abdullah012039@users.noreply.github.com>
2024-08-24 00:27:05 +0300

    Delete src directory

--- src/Section.jsx
+++
@@ -1,9 +0,0 @@
-import style from './page.module.css';
-const Section = ({isDarkMode}) => {
-    return (
-        <div  className={isDarkMode ? style.darkMode : style.lightMode}>
-            <h1>Section</h1>
-        </div>
-    );
-}
-export default Section;
